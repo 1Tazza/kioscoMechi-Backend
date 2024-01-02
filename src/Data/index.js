44c3747d1218f517ecee60808b9692b2abd502aa
unknown <tazza.personal@gmail.com>
2024-01-02 00:40:41 -0300

    functional backend

---
+++ src/Data/index.js
@@ -0,0 +1,69 @@
+const json = { data: [{
+    name: "Pan Rallado",
+    price: 1000,
+    brand: ["Preferido"],
+    quantity: "",
+    img:"https://kiosko-mechi.s3.us-east-2.amazonaws.com/Productos-Almacen/Pan+Rallado.png",
+    categories: ["almacen"],
+    company: [""]
+},{
+    name: "Queso Rallado Grande",
+    price: 1800,
+    brand: ["Serenisima"],
+    quantity: "",
+    img:"https://kiosko-mechi.s3.us-east-2.amazonaws.com/Productos-Almacen/queso+rallado+grande.jpg",
+    categories: ["almacen"],
+    company: ["Serenisima"]
+},{
+    name: "Pan Dulce",
+    price: 2500,
+    brand: ["Arcor"],
+    quantity: "",
+    img:"https://kiosko-mechi.s3.us-east-2.amazonaws.com/Productos-Almacen/Arcor+Pan+Dulce.png",
+    categories: ["almacen"],
+    company: [""]
+},{
+    name: "Termo",
+    price: 1000,
+    brand: ["Taragui"],
+    quantity: "",
+    img:"https://kiosko-mechi.s3.us-east-2.amazonaws.com/Productos-Almacen/termo+taragui.jpg",
+    categories: ["almacen"],
+    company: [""]
+},{
+    name: "Queso Rallado Pequeño",
+    price: 800,
+    brand: ["Taragui"],
+    quantity: "",
+    img:"https://kiosko-mechi.s3.us-east-2.amazonaws.com/Productos-Almacen/queso+rallado+peque%C3%B1o+la+serenisima.png",
+    categories: ["almacen"],
+    company: [""]
+},{
+    name: "Yerba Mini",
+    price: 500,
+    brand: ["Taragui"],
+    quantity: "500g",
+    img:"https://kiosko-mechi.s3.us-east-2.amazonaws.com/Productos-Almacen/Yerba+taragui+mini.png",
+    categories: ["almacen"],
+    company: [""]
+},{
+    name: "Sobres Endulzantes Edulcorante",
+    price: 1200,
+    brand: ["Hileret"],
+    quantity: "",
+    img:"https://kiosko-mechi.s3.us-east-2.amazonaws.com/Productos-Almacen/sobres+de+endulzante+hileret.jpg",
+    categories: ["almacen"],
+    company: [""]
+},{
+    name: "Yerba Mini",
+    price: 2300,
+    brand: ["Playadito"],
+    quantity: "500g",
+    img:"https://kiosko-mechi.s3.us-east-2.amazonaws.com/Productos-Almacen/Yerba+playadito+mini.png",
+    categories: ["almacen"],
+    company: [""]
+}]}
+
+
+
+module.exports = {json}
---
+++ src/models/Product.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('product', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      allowNull: true,
+    },
+    brand: {
+      type: DataTypes.ARRAY(DataTypes.JSON),
+      allowNull: true,
+      defaultValue: []
+    },
+    img: {
+      type: DataTypes.STRING,
+      defaultValue: "",
+      allowNull: true
+    },
+    quantity: {
+      type: DataTypes.STRING,
+      defaultValue: "",
+      allowNull: true
+    },
+    categories: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+        allowNull: true,
+        defaultValue: []
+    },
+    company: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+        allowNull: true,
+        defaultValue: []
+    }
+  }, {
+    timestamps: false
+  });
+};
